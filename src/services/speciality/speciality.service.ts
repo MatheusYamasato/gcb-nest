25711fa4c1fc97819d60d687409649260e354e5d
MatheusYamasato <matheus-yamasato@hotmail.com>
2021-06-17 19:43:48 -0300

    add docker-compose

--- src/services/speciality/speciality.service.ts
+++ src/services/speciality/speciality.service.ts
@@ -1,4 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { Speciality } from 'src/specialities/speciality';
+import {v4 as uuid} from 'uuid';
 
 @Injectable()
-export class SpecialityService {}
+export class SpecialityService {
+    private specialities: Speciality[] = [];
+    
+    public getAll(): Speciality[] {
+        return this.specialities;
+    }
+
+    public create(speciality: Speciality): string {
+        try {
+            if (speciality.id === undefined) {
+                speciality.id = uuid();
+            }
+            this.specialities.push(speciality);
+            return speciality.id;
+        } catch {
+            return "";
+        }
+    }
+
+}
--- src/specialities/specialities.controller.ts
+++ src/specialities/specialities.controller.ts
@@ -1,4 +1,27 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Body, Post, Get, HttpException, HttpStatus } from '@nestjs/common';
+import { SpecialityService } from 'src/services/speciality/speciality.service';
+import { Speciality } from './speciality';
 
 @Controller('specialities')
-export class SpecialitiesController {}
+export class SpecialitiesController {
+    constructor(private readonly specialityService: SpecialityService) {
+    }
+ 
+    // Adiciono um novo blog
+    @Post()
+    // @Body significa que vou pegar o objeto no corpo da requisição
+    public create(@Body() speciality: Speciality): string {
+        const result = this.specialityService.create(speciality);
+ 
+        if (result === "")
+            throw new HttpException("Not Found", HttpStatus.NOT_FOUND);
+ 
+        return result;
+    }
+ 
+    // Retorno todos os docs
+    @Get()
+    public getAll(): Speciality[] {
+        return this.specialityService.getAll();
+    }
+}
