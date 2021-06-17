f99ad9292cbfaac025af0569729e9a130e90ee67
MatheusYamasato <matheus-yamasato@hotmail.com>
2021-06-17 20:11:01 -0300

    typeorm implementando com colunas e relacionamento

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm'
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DoctorsController } from './doctors/doctors.controller';
@@ -9,7 +10,7 @@ import { SpecialityService } from './services/speciality/speciality.service';
 import { SpecialitiesModule } from './specialities/specialities.module';
 
 @Module({
-  imports: [DoctorsModule, SpecialitiesModule],
+  imports: [DoctorsModule, SpecialitiesModule, TypeOrmModule.forRoot()],
   controllers: [AppController, DoctorsController, SpecialitiesController],
   providers: [AppService, DoctorService, SpecialityService],
 })
